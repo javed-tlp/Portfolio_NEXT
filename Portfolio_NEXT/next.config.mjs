2abffef26c1307a209a8e2edc39b02ec7ce405b2
javed-tlp <saifijaved616@gmail.com>
2026-05-26 23:49:58 +0530

    final fix

--- Portfolio_NEXT/next.config.mjs
+++ Portfolio_NEXT/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
+  turbopack: {},
+
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
     return config;
   },
 };
