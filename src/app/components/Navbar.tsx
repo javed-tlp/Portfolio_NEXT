05ecbb506092e6115ab3b8144bb05734d6156f8c
javed-tlp <saifijaved616@gmail.com>
2026-05-27 00:47:33 +0530

    changes donee

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,8 @@
-"use client"; // Mark this file as a Client Component
+"use client";
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faStar, faBriefcase, faGraduationCap, faTools, faDownload, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FaStar, FaHome, FaBriefcase, FaTools, FaGraduationCap, FaDownload } from "react-icons/fa";
 import styles from "./navbar.module.css";
 
 const Navbar: React.FC = () => {
@@ -30,39 +29,37 @@ const Navbar: React.FC = () => {
       <ul className={`${styles["nav-links"]} ${menuOpen ? styles["show"] : styles["hide"]}`}>
         <li onClick={closeMenu}>
           <Link href="/skills">
-            <FontAwesomeIcon icon={faStar} className={styles["nav-icon"]} />
+            <FaStar className={styles["nav-icon"]} />
             Skills
           </Link>
         </li>
         <li onClick={closeMenu}>
           <Link href="/projects">
-            <FontAwesomeIcon icon={faHome} className={styles["nav-icon"]} />
+            <FaHome className={styles["nav-icon"]} />
             Projects
           </Link>
         </li>
         <li onClick={closeMenu}>
           <Link href="/experience">
-            <FontAwesomeIcon icon={faBriefcase} className={styles["nav-icon"]} />
+            <FaBriefcase className={styles["nav-icon"]} />
             Experience
           </Link>
         </li>
-        
-        
         <li onClick={closeMenu}>
           <Link href="/tools">
-            <FontAwesomeIcon icon={faTools} className={styles["nav-icon"]} />
+            <FaTools className={styles["nav-icon"]} />
             Tools
           </Link>
         </li>
         <li onClick={closeMenu}>
           <Link href="/education">
-            <FontAwesomeIcon icon={faGraduationCap} className={styles["nav-icon"]} />
+            <FaGraduationCap className={styles["nav-icon"]} />
             Education
           </Link>
         </li>
         <li onClick={closeMenu}>
           <a href="/JavedSaifi_Resume.pdf" download className={styles["cv-link"]}>
-            <FontAwesomeIcon icon={faDownload} className={styles["nav-icon"]} />
+            <FaDownload className={styles["nav-icon"]} />
             Download CV
           </a>
         </li>
